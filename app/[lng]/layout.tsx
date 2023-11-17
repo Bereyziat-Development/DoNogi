0fb416047f3a4154fbee96bb14860537fc3b52d9
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2023-11-17 13:36:22 +0100

    chores: change favicon icon for donogi

--- app/[lng]/layout.tsx
+++ app/[lng]/layout.tsx
@@ -53,6 +53,8 @@ export default function RootLayout({
 
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+
         <title>Do Nogi</title>
       </head>
       <body suppressHydrationWarning={true}>
